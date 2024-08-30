752731fded8518449978b96e8c3792f5a1abdb58
aulafirdaus <aulafirdaus2945@gmail.com>
2024-08-30 14:52:27 +0700

    menghilangkan banner tulisan debug yang di pojok kanan atas

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -10,6 +10,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         // appBar: AppBar(
         //   title: const Text('Home'),
